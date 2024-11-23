d6552187e1e027f38a5d1c95b5501b350e0c4756
Rushi_Borkar <borkarananta028@gmail.com>
2024-11-23 17:38:56 +0530

    Fixed SMTP code

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'borkarananta028@gmail.com'
-EMAIL_HOST_PASSWORD = 'uxntmozdvmtqlgtl'
+EMAIL_HOST_USER = ''        # Add your email here
+EMAIL_HOST_PASSWORD = ''    # Add your app password here
